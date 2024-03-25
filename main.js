3c1890119e30246cf86d29dd3486a394bb6cf5ef
TSCarol <thaiscsantoos@gmail.com>
2024-03-25 16:08:18 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const nameElement = document.querySelector('#name');
+    const userName = document.querySelector('#username');
+    const avatarElement = document.querySelector('#avatar');
+    const reposElement = document.querySelector('#repos');
+    const followersElement = document.querySelector('#followers');
+    const followingElement = document.querySelector('#following');
+    const linkElement = document.querySelector('#link');
+
+    fetch('https://api.github.com/users/ogiansouza').then(function(response) {
+            if (!response.ok) {
+                throw new Error('Não foi possível carregar os dados do usuário.');
+            }
+            return response.json();
+    })
+    .then(function(json) {
+        nameElement.innerText = json.name;
+        userName.innerText = json.login;
+        avatarElement.src = json.avatar_url;
+        reposElement.innerText = json.public_repos;
+        followersElement.innerText = json.followers;
+        followingElement.innerText = json.following;
+        linkElement.href =json.html_url;
+    })
+    .catch(function(erro) {
+        alert('Ocorreu um erro:', error);
+    }) 
+})
+
